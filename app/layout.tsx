1979f788f2de1fc86ba44b9d34af33fa3750de2f
Chandru Suresh <sureshchandru2003@gmail.com>
2024-10-09 20:27:03 +0100

    add: social image

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,23 @@ export const metadata: Metadata = {
   description:
     "The portfolio site of Chandru Suresh; British software engineer and computer programmer",
   keywords: "developer,portfolio,software",
+  openGraph: {
+    title: "Chandru Suresh",
+    description:
+      "The portfolio site of Chandru Suresh; British software engineer and computer programmer",
+    url: "https://chandru-su.com",
+    siteName: "Chandru Suresh Portfolio",
+    type: "website",
+    images: [
+      {
+        url: "https://chandru-su.com/metaimage.png",
+        secureUrl: "https://chandru-su.com/metaimage.png",
+        width: 1142,
+        height: 656,
+        alt: "Preview image for Chandru Suresh",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
